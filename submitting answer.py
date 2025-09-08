1edd9ae5c13466185086b42c4c95a6453e8ea936
Tom Duquennoy <tom.duquennoy.pro@gmail.com>
2025-09-08 16:44:16 +0200

    panda intro

--- submitting answer.py
+++ submitting answer.py
@@ -1,8 +1,8 @@
 import math
 import requests
 
-data = {"number": "180",
-        "answer": "argmax",
+data = {"number": "193",
+        "answer": "(63,47,24)",
         "user": "t.duquennoy"}
 r = requests.post("http://34.163.196.38/", data=data)
 print(r.text)
