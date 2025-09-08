865adebbf307d9f59aabd84eb132ea092c17c7d9
Tom Duquennoy <tom.duquennoy.pro@gmail.com>
2025-09-08 15:39:11 +0200

    numpy intro

--- submitting answer.py
+++ submitting answer.py
@@ -1,10 +1,13 @@
 import math
 import requests
 
-data = {"number": "161",
-        "answer": "(2, 3)",
+data = {"number": "180",
+        "answer": "argmax",
         "user": "t.duquennoy"}
 r = requests.post("http://34.163.196.38/", data=data)
 print(r.text)
+
+
+
 
 
